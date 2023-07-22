0265828cb3d19373b7b8ef56f0fb314453e28d86
GiantDole <aak2229@columbia.edu>
2023-07-22 02:56:17 +0200

    mock account

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -3,10 +3,10 @@ import { DeployFunction } from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const accounts = await hre.getUnnamedAccounts();
-  await hre.deployments.deploy('Greeter', {
+  await hre.deployments.deploy('SimpleAccount', {
     from: accounts[0],
     deterministicDeployment: true,
-    args: ['Test'],
+    args: ['0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789'],
     log: true,
   });
 };
